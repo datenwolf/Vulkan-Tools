48fda60030b86a7273c0104b4643dcfb125cddcf
Shannon McPherson <shannon@lunarg.com>
2020-04-10 16:14:29 -0600

    build: Update known-good files for 1.2.137 header
    Changes:
    - Updated known-good files
    - Generated new source files
    - Removed `VULKAN_HPP_NO_SMART_HANDLE` definition for `cube.cpp`
    
    Change-Id: Icb0152455f2c0b21a29d70462d05b958c53be861

--- cube/cube.cpp
+++ cube/cube.cpp
@@ -34,7 +34,7 @@
 #include <sstream>
 #include <memory>
 
-#define VULKAN_HPP_NO_SMART_HANDLE
+
 #define VULKAN_HPP_NO_EXCEPTIONS
 #define VULKAN_HPP_TYPESAFE_CONVERSION
 #include <vulkan/vulkan.hpp>
